02f5a8089b23e9576a29b9950fbdf1d293dab311
mohammad2003mrz <gorbanben10@gmail.com>
2023-06-06 19:54:43 +0430

    Challenge--4

--- Day-3-Challenge.js
+++ Day-3-Challenge.js
@@ -19,3 +19,17 @@ console.log(10 === "10");
 // Challenge--3
 
 console.log(parseInt("9.8") === 10);
+
+//Challenge--4
+
+// True Value
+
+console.log(6 > 5);
+console.log(6 > 5 && undefined !== 10);
+console.log(typeof Nan === typeof number);
+
+// False Value
+
+console.log(10 > 11);
+console.log(!(6 > 5) && undefined !== 10);
+console.log("Alireza".length === "Mohammadreza".length);
