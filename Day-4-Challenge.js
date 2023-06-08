b3f337308b4c54e91653ede750c1c7972e3d1be8
mohammad2003mrz <gorbanben10@gmail.com>
2023-06-08 08:58:41 +0430

    Challenge--1

--- Day-4-Challenge.js
+++ Day-4-Challenge.js
@@ -1,1 +1,14 @@
 "use strict";
+
+//Exercises: Level 1
+
+//Challenge--1
+
+const enterUserInput = Number(prompt("Please Enter Your Age"));
+
+if (enterUserInput >= 18) {
+  console.log("You are old enough to drive");
+} else {
+  const elseEnter = 18 - enterUserInput;
+  console.log(`You are left with ${elseEnter} years to drive.`);
+}
