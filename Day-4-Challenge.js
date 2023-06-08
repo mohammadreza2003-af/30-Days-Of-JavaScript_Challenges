4053451c1a351ec2783d3fe5f343df2a0edb345b
mohammad2003mrz <gorbanben10@gmail.com>
2023-06-08 12:33:34 +0430

    challenge--2

--- Day-4-Challenge.js
+++ Day-4-Challenge.js
@@ -12,3 +12,16 @@ if (enterUserInput >= 18) {
   const elseEnter = 18 - enterUserInput;
   console.log(`You are left with ${elseEnter} years to drive.`);
 }
+
+//challenge--2
+
+const inputUser1 = Number(prompt("Enter Your Age 1"));
+const inputUser2 = Number(prompt("Enter Your Age 2"));
+
+inputUser1 > inputUser2
+  ? console.log(
+      `User 1 (${inputUser1})Years is Biger User 2 (${inputUser2})Years`
+    )
+  : console.log(
+      `User 2 (${inputUser2})Years is Biger User 1 (${inputUser1})Years`
+    );
