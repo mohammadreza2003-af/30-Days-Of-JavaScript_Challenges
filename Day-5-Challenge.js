790f60163709be0ff9ca4df6602cf9342da0405d
mohammad2003mrz <gorbanben10@gmail.com>
2023-06-15 09:05:30 +0430

    Challenge--3

--- Day-5-Challenge.js
+++ Day-5-Challenge.js
@@ -149,3 +149,24 @@ const words = remove.split(" ");
 // console.log(remove);
 console.log(words);
 console.log(words.length);
+
+//Challenge--3
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+if (!shoppingCart.includes("Meat")) {
+  shoppingCart.unshift("Meat");
+}
+if (!shoppingCart.includes("Sugar")) {
+  shoppingCart.push("Sugar");
+}
+const allgHoney = true;
+if (allgHoney && shoppingCart.includes("Honey")) {
+  const indexHoney = shoppingCart.indexOf("Honey");
+  shoppingCart.splice(indexHoney, 1);
+}
+const teaIndex = shoppingCart.indexOf("Tea");
+if (teaIndex === -1) {
+  shoppingCart[teaIndex] = "Green Tea";
+}
+console.log(shoppingCart);
