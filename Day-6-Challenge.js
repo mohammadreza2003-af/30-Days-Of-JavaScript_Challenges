ef8ad2840417a28f5100074928b5679ba28f0e46
mohammad2003mrz <gorbanben10@gmail.com>
2023-07-03 21:22:33 +0430

    Challenge--1

--- Day-6-Challenge.js
+++ Day-6-Challenge.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//Exercise: Level 1
+
+// Challenge--1
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+let j = 0;
+while (j <= 10) {
+  console.log(j);
+  j++;
+}
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k <= 10);
