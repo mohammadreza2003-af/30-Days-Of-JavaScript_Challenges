955a2965d4a0dddd6c4d5e896178e3da1ebe9260
mohammad2003mrz <gorbanben10@gmail.com>
2023-07-12 19:44:34 +0430

    Challenge--1 & Challenge--2

---
+++ Day-7-Challenge.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Exercise: Level 1
+
+// Challenge--1 & Challenge--2
+
+const fullName = function () {
+  const name = "Ali";
+  const lastName = "Ebrahimi";
+  const fullName = `${name} ${lastName}`;
+  return fullName;
+};
+
+console.log(fullName());
+
+
