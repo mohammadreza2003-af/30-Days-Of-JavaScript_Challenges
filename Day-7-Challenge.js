5ab5fe07a71c9889a277eaa686368d9c98514c99
mohammad2003mrz <gorbanben10@gmail.com>
2023-07-12 19:49:32 +0430

    Challenge--4

--- Day-7-Challenge.js
+++ Day-7-Challenge.js
@@ -16,9 +16,15 @@ console.log(fullName());
 //Challenge--3
 
 const addNumbers = (num1, num2) => {
-    return num1 + num2;
+  return num1 + num2;
 };
 
 console.log(addNumbers(2, 3));
 
+//Challenge--4
 
+const areaReg = function (length, width) {
+  const area = length * width;
+  return area;
+};
+console.log(areaReg(2, 2));
