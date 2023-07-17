584d0c0d47b50ff4cd32ec4f48fa32d0c264fa87
mohammad2003mrz <gorbanben10@gmail.com>
2023-07-17 09:43:09 +0430

    Challenge--2

--- Day-8-Challenge.js
+++ Day-8-Challenge.js
@@ -32,3 +32,104 @@ console.log(dog.getDogInfo());
 
 //Exercises: Level 2
 
+//Challenge--1
+
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+let maxSkills = 0;
+let skillfulPerson = "";
+
+for (const person in users) {
+  const skillsCount = users[person].skills.length;
+  if (skillsCount > maxSkills) {
+    maxSkills = skillsCount;
+    skillfulPerson = person;
+  }
+}
+
+console.log(
+  `The person with the most skills is ${skillfulPerson} : ${maxSkills}`
+);
+
+//Challenge--2
+
+let loggedInCount = 0;
+let greaterThan50PointsCount = 0;
+
+for (const user in users) {
+  if (users[user].isLoggedIn) {
+    loggedInCount++;
+  }
+  if (users[user].points >= 50) {
+    greaterThan50PointsCount++;
+  }
+}
+console.log("Logged-in Users Count:", loggedInCount);
+console.log("Users with Points >= 50 Count:", greaterThan50PointsCount);
