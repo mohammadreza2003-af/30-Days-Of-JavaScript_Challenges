260d94a08dddfe1040405ee454bfae369d4b1f01
mohammad2003mrz <gorbanben10@gmail.com>
2023-08-22 21:28:18 +0430

    Challenge--1

--- Day-9-Challenge.js
+++ Day-9-Challenge.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const countries = ["Finland", "Sweden", "Denmark", "Norway", "IceLand"];
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const products = [
+  { product: "banana", price: 3 },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: 8 },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+//Challenge--1
+
+console.log(numbers.map((cur, i) => `${i + 1} => ${cur}`));
+console.log(numbers.filter((cur) => cur >= 5));
+console.log(numbers.reduce((acc, cur) => acc + cur, 0));
+
